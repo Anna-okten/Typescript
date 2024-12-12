9bf5852ae211bb2dd5915270e75bb7071a21792b
Anna-okten <vladdomeshkin@gmail.com>
2024-12-12 13:11:59 +0200

    add ts

---
+++ demos/lesson1/main1.ts
@@ -0,0 +1,58 @@
+// task 1
+let word1: string = "hello";
+console.log(word1);
+
+let word2: string = "owu";
+console.log(word2);
+
+let word3: string = 'com';
+console.log(word3);
+
+let word4: string = 'ua';
+console.log(word4);
+
+let num1: number = 1;
+console.log(num1);
+
+let num2: number = 10;
+console.log(num2);
+
+let num3: number = -999;
+console.log(num3);
+
+let num4: number = 123;
+console.log(num4);
+
+let num5: number = 3.14;
+console.log(num5);
+
+let num6: number = 2.7;
+console.log(num6);
+
+let num7: number = 16;
+console.log(num7);
+
+let boolean1: boolean = true;
+console.log(boolean1);
+
+let boolean2: boolean = false;
+console.log(boolean2);
+
+
+// // task 2
+
+let firstName: string = 'Anna';
+let middleName: string = 'Valerievna';
+let lastName: string = 'Domeshkina';
+let person: string = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+
+// // task 3
+
+let a: number=100
+console.log(typeof a);
+let b: string='100';
+console.log(typeof b);
+let c: boolean =true;
+console.log(typeof c);
